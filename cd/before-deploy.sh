ebc45d1b9f9abf7e001190985b89471daebcd659
Antonin Stoklasek <antonin.stoklasek@bsc-ideas.com>
2018-01-24 20:16:00 +0100

    fix naming

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'dev' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_e6ab63f51501_key -iv $encrypted_e6ab63f51501_iv -in codesigning.asc.enc -out codesigning.asc -d
-    gpg --fast-import cd/signingkey.asc
+    gpg --fast-import cd/codesigning.asc
 fi
