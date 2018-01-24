5d1edd5f9899367b1e4e55a4d0db6dcb05b3fe94
Antonin Stoklasek <antonin.stoklasek@bsc-ideas.com>
2018-01-24 20:19:49 +0100

    fix paths

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'dev' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_e6ab63f51501_key -iv $encrypted_e6ab63f51501_iv -in codesigning.asc.enc -out codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_e6ab63f51501_key -iv $encrypted_e6ab63f51501_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
     gpg --fast-import cd/codesigning.asc
 fi
