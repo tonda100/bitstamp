cb4e8efba0dc9e8298d475318b392aebc5774d9f
Antonin Stoklasek <antonin.stoklasek@bsc-ideas.com>
2018-01-24 20:10:32 +0100

    rename back

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'dev' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
+    openssl aes-256-cbc -K $encrypted_e6ab63f51501_key -iv $encrypted_e6ab63f51501_iv -in codesigning.asc.enc -out codesigning.asc -d
     gpg --fast-import cd/signingkey.asc
 fi
